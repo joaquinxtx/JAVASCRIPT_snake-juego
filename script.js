56a64bb2ac1ae98016e7fd6295b97942738eb5fe
joaquinxtx <thejotex1998@gmail.com>
2022-03-20 23:02:04 -0300

    proyacto snake

--- script.js
+++ script.js
@@ -1,42 +1,43 @@
+//canvas y contexto 2d
 const lienzo = document.getElementById("lienzo")
 const ctx=lienzo.getContext("2d")
 
+//dibujosnake snake +posiciones
 let posX = 50 , posY=50;
-let direccionX=1;
-let direccionY=0;
-let velocidadX = 1;
-let velocidadY = 0.6;
 
-
-function draw(){
-  ctx.beginPath()
-  bordesCanvas()
-  personajeSnake()
-  ctx.clearRect(0,0,lienzo.width,lienzo.height)
-  ctx.stroke();
-  direccionesSnake(posX,posY)
-}
-
-//tamaño de snake
 function personajeSnake(){
   ctx.rect(posX,posY,10,5);
 }
+
+//paredes(tope) y velocidad snake 
+let direccionX=1;//direccion de los ejes
+let direccionY=0;
+let velocidadX = 1;
+let velocidadY = 0.6;
 function direccionesSnake(){
   if(posX >= 5 && posX <= (300-16)){
     posX= posX +direccionX*velocidadX
+    
+  }else{
+    console.log('juego terminado')
+    noLoop()
   }
   
   if(posY >= 5 && posY <= (138)){
     posY = posY +direccionY*velocidadY
-  }
-  
-  
+  }else{
+    console.log('juego terminado')
+    noLoop()
+  } 
 }
+
+//sistemas de choques 
+
 //movimientos
 function keyPressed(){
   switch(keyCode){
     case LEFT_ARROW:
-      posX -= 1
+      posX -= 1;
       direccionX=-1;
       direccionY=0;
       break;
@@ -51,13 +52,13 @@ function keyPressed(){
       direccionX=0;
       break; 
     case DOWN_ARROW:
-      posY += 1
+      posY += 1;
       direccionY=+1;
       direccionX=0;
       break;    
   }
 }
-//paredes del canvas
+//paredes (dibujadas)
 function bordesCanvas(){
   for(let iX = 0 ; iX<300 ; iX+=5){
     lineasBorde(iX,0)
@@ -76,7 +77,35 @@ function lineasBorde(x,y){
   ctx.rect(x,y,5,5)
 }
 
+//comida snake 
+function comida(x,y){
+  ctx.rect(x,y,10,5)  
+}
+function comidaSnake(){
+  comida(50 ,70)
+}
 
+
+//enemigo snake
+function enemigo(x,y){
+  ctx.triangle(x, y,  5, 5, 5);
+}
+function enemigoSnake(){
+  enemigo(70,65,75)
+}
+
+//loop del juego
+function draw(){
+  ctx.beginPath();
+  comidaSnake();
+  
+  bordesCanvas();
+  personajeSnake();
+  ctx.clearRect(0,0,lienzo.width,lienzo.height);
+  ctx.stroke();
+  direccionesSnake(posX,posY);
+  
+}
 
 const formularios = document.getElementById('formulario')
 const botonUser = document.getElementById('botonId')
